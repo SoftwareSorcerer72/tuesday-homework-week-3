81feaed6c4547ebd3311bc5e2f93b3105a892a04
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 03:24:09 -0400

    Added function to developer class. Made a dev named James Doe Jr. to test.

--- homeworktest.py
+++ homeworktest.py
@@ -11,7 +11,7 @@ class Employee:
         self.email = email.lower()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.job_title}"
+        return f"{self.first_name} {self.last_name}"
 
     def give_raise(self):
         self.salary = (self.salary * Employee.raise_amount)
@@ -21,13 +21,17 @@ e =(Employee("john", "doe", "developer", "somethingrandom@gmail.com", 52000))
 
 e.give_raise()
 
-class Sales(Employee)
+class Sales(Employee):
     def __init__(self, first_name, last_name, job_title, email, salary, phone_number):
         super().__init__(first_name, last_name, job_title, email, salary)
         self.phone_number = phone_number
-        
-    
 
+    def send_followup_email(self, customer_name):
+        print(f"\tDear {customer_name}, \n Thank you for your interest in our product. Please let me know if you have any questions. My email is {self.email} and my phone number is {self.phone_number}. \n\n Thanks, \n {self}")
 
 class Developer(Employee):
-    
+    def write_code(self):
+        print(f"{self} is writing code!")
+
+d = Developer('James', 'Doe Jr.', 'Software Developer', 'john.doe@example.com', 70000)
+d.write_code()
