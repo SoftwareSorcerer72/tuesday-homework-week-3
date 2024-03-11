f9818d38302dd409c31974433e711357a5054930
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-11 02:59:36 -0400

    Looks like I got the first problem done. On to the next.

--- homeworktest.py
+++ homeworktest.py
@@ -10,5 +10,13 @@ class Employee:
         self.salary = salary
         self.email = email.lower()
 
-        def give_raise(self):
-            self.salary = (self.salary * Employee.raise_amount)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.job_title}"
+
+    def give_raise(self):
+        self.salary = (self.salary * Employee.raise_amount)
+        print(f"Good job {self.first_name}! You've earned a raise! Your new salary is {self.salary}.")
+
+e =(Employee("john", "doe", "developer", "somethingrandom@gmail.com", 52000))
+
+e.give_raise()
